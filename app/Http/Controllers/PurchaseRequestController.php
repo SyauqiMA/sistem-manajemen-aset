84a45887e4b3a4c87940a3c5777cd778854fd9e6
SyauqiMA <syauqiabrar@gmail.com>
2024-04-08 16:55:06 +0700

    implemented controller functions for PR approvals

--- app/Http/Controllers/PurchaseRequestController.php
+++ app/Http/Controllers/PurchaseRequestController.php
@@ -47,7 +47,7 @@ class PurchaseRequestController extends Controller
     }
 
     /**
-     * View PR Data untuk Direktur
+     * View Received PR untuk Direktur
      */
     public function direkturShow(Request $request): View {
         // get all purchase request with status=1
@@ -64,18 +64,79 @@ class PurchaseRequestController extends Controller
     }
 
     /**
-     * Reject Purchase Request
+     * View Received PR untuk Man.Procurement
      */
-    public function rejectPurchaseRequest(Request $request): void {
-        // TODO
-        return;
+    public function manProcShow(Request $request): View {
+        // get all purchase requests with status=2
+        $rows = DB::table('purchase_request')
+                ->select(['id',
+                          'purchase_request_number',
+                          'purchase_request_name',
+                          'purchase_request_date',
+                          'id_user'])
+                ->where('status', '=', 2)
+                ->get();
+        
+        return view('received-purchase-requests', ['rows' => $rows]);
     }
 
     /**
-     * Accept Purchase Request
+     * Reject Purchase Request for Direktur
      */
-    public function acceptPurchaseRequest(Request $request): void {
-        // TODO
-        return;
+    public function rejectPRDirektur(Request $request): RedirectResponse {
+        // get id
+        $pr_id = $request->input('id');
+
+        // update the PR in table
+        $updated_pr = DB::table('purchase_request')
+                            ->where('id', '=', $pr_id)
+                            ->update(['status' => 4]); // 4 = Rejected by Direktur 
+
+        return back()->with('status', 'Berhasil reject Purchase Request');
+    }
+
+    /**
+     * Accept Purchase Request for Direktur
+     */
+    public function acceptPRDirektur(Request $request): RedirectResponse {
+        // get id
+        $pr_id = $request->input('id');
+
+        // update the PR in table
+        $updated_pr = DB::table('purchase_request')
+                          ->where('id', '=', $pr_id)
+                          ->update(['status' => 2]); // 2 = Approved by Direktur 
+
+        return back()->with('status', 'Berhasil approve Purchase Request');
+    }
+
+    /**
+     * Reject Purchase Request for Man.Procurement
+     */
+    public function rejectPRManProc(Request $request): RedirectResponse {
+        // get id
+        $pr_id = $request->input('id');
+
+        // update the PR in table
+        $updated_pr = DB::table('purchase_request')
+                            ->where('id', '=', $pr_id)
+                            ->update(['status' => 5]); // 5 = Rejected by Man.Procurement 
+
+        return back()->with('status', 'Berhasil reject Purchase Request');
+    }
+
+    /**
+     * Accept Purchase Request for Man.Procurement
+     */
+    public function acceptPRManProc(Request $request): RedirectResponse {
+        // get id
+        $pr_id = $request->input('id');
+
+        // update the PR in table
+        $updated_pr = DB::table('purchase_request')
+                          ->where('id', '=', $pr_id)
+                          ->update(['status' => 3]); // 3 = Approved by Man.Procurement 
+
+        return back()->with('status', 'Berhasil approve Purchase Request');
     }
 }
